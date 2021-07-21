465acb09d65b15b0cad2a8cdc51dd2a7c09d00cc
Samuel Wahba (Mr_Zebulon) <samwahba3@gmail.com>
2021-07-21 18:28:50 +0200

    same

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+from re import L
 import pandas as pd
 from placekey.api import PlacekeyAPI
 import enlighten
-import math, pathlib, os, json, time
+import math
 from concurrent.futures.thread import ThreadPoolExecutor
-from concurrent.futures import as_completed
+
+pd.options.mode.chained_assignment = None
+
 #PARAMS
 
 COL_OFFSET = 4
@@ -16,7 +19,7 @@ PATH_OUT= "./out"
 PATH_TEMP= "./temp"
 
 FILE_IN = f'{PATH_IN}/{COUNTRY}.csv'
-FILE_OUT = f'{PATH_OUT}/{COUNTRY}_out.csv'
+FILE_OUT = f'{PATH_OUT}/{COUNTRY}_' + '{0}' + '_out.csv'
 
 ROWS = 1000
 BATCH_SIZE = 250
@@ -32,55 +35,34 @@ PROGRESS_BARS = enlighten.get_manager()
 
 #Functions & Classes
 
-class TempWrapper():
+def process(index, batch, data, progression):
     
-    def __init__(self):
-        self.path = f'{PATH_TEMP}/computed.json'
-
-        with open(self.path, 'w+') as f:
-
-            if os.stat(self.path).st_size == 0:
-                json.dump({}, f)
-                self.blocks = dict()
-            else:
-                self.blocks = json.load(f)
-    
-    def get_block(self, id):
-        try:
-            return self.blocks[str(id)]
-        except:
-            return None
-
-    def new_block(self, block):
-        id = block[0]['query_id']
-        self.blocks[id] = block
-
-    def save(self):
-        with open(self.path, 'w') as f:
-            json.dump(self.blocks, f)
-
-def process(batch, wrapper, progression):
-        
-    block = wrapper.get_block(batch[0]['query_id'])
-
-    if block is not None:
-        return block
-
     lookup = list()
 
     try:
         lookup = API.lookup_placekeys(batch)
 
     except:
-        for _ in len(batch):
+        for entry in len(batch):
             lookup.append({'query_id' : entry['query_id'], 'error': 'Unknown error'})
     
     progression.update()
 
-    return lookup 
+    for entry in lookup:
+        try:
+            data["placekey"] = lookup['placekey']
+        except:
+            data["placekey"] = 'Error'
+
+    data.to_csv(FILE_OUT.format(index), sep="|")
+
 
 def get_batch(input, index):
     return input[index * BATCH_SIZE:(i+1) *BATCH_SIZE]
+
+
+def get_sub_dataframe(dataframe, index):
+    return dataframe.iloc[index * BATCH_SIZE :(index + 1) * BATCH_SIZE, :]
 
 #Execution
 
@@ -100,36 +82,12 @@ for index, entry in df.iterrows():
         "iso_country_code": ISO_CODE
     }))
 
-futures = []
-
-processing_progression_bar = PROGRESS_BARS.counter(total = ROWS, desc="Processing", unit="entries", color="white")
+processing_progression_bar = PROGRESS_BARS.counter(total = BATCHES, desc="Processing", unit="entries", color="white")
 
 with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
 
-    wrapper = TempWrapper()
     for i in range(BATCHES):
-        futures.append(executor.submit(process, get_batch(data, i), wrapper, processing_progression_bar))
+        executor.submit(process, i, get_batch(data, i), get_sub_dataframe(df, i), processing_progression_bar)
         
-
-res = list()
-for future in as_completed(futures):
-    for iteration in future.result():
-        res.append(iteration)
-
-values = list()
-for i, e in enumerate(sorted(res, key=lambda x: x['query_id'])):
-    try:
-        values.append(e['placekey'])
-    except KeyError:
-        try:
-            values.append(e['error'])
-        except KeyError:
-            values.append('')
-
-df["placekey"] = values
-
-df.to_csv(FILE_OUT)
-
-[f.unlink() for f in pathlib.Path(PATH_TEMP).glob("*") if f.is_file()] 
 
 PROGRESS_BARS.stop()
